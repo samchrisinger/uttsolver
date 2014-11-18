8ae8fb9d69385f168b9cfc1826667ef59fee01d9
Sam Chrisinger <s.chrisinger@gmail.com>
2014-11-18 15:16:05 -0500

    Making build script safer

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-rm src/coq/*.vo
+rm ./src/coq/*.vo
 
-cd src/coq
-coqc types.v
-coqc game.v
-coqc build.v
+cd ./src/coq
+coqc ./types.v
+coqc ./game.v
+coqc ./build.v
 coqc -byte build
 
 mv *.hs ../../src/hs/
